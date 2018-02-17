058e0f8d86fc0e944f4d273e4a210750d8cddd38
rvsrafael <rvs.contato@gmail.com>
2018-02-17 00:01:16 -0200

    start do app

---
+++ Assets/Script/Inicio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Return) || Input.touchCount == 2) {
+			SceneManager.LoadScene ("game");
+		}	
+	}
+}

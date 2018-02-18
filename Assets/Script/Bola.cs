578e7d379cd1a44a55dd87ce0e01309a7853b241
rvsrafael <rvs.contato@gmail.com>
2018-02-17 22:57:50 -0200

    logica da direcao da bola

--- Assets/Script/Bola.cs
+++ Assets/Script/Bola.cs
@@ -8,4 +8,25 @@ public class Bola : MonoBehaviour {
 	void Start () {
 		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
 	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		// Hit the Racket?
+		if (col.gameObject.name == "raquete") {
+			// Calculate hit Factor
+			float x = hitFactor(transform.position,
+				col.transform.position,
+				col.collider.bounds.size.x);
+
+			// Calculate direction, set length to 1
+			Vector2 dir = new Vector2(x, 1).normalized;
+
+			// Set Velocity with dir * speed
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos,
+		float racketWidth) {
+		return (ballPos.x - racketPos.x) / racketWidth;
+	}
 }

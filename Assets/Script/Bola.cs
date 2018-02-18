2bc2dfaf4b43c1e52309f113d8fe001e884a671a
rvsrafael <rvs.contato@gmail.com>
2018-02-17 22:41:09 -0200

    raquete e ball

---
+++ Assets/Script/Bola.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+	public float speed = 100.0f;
+
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+	}
+}
---
+++ Assets/Script/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		print("passou");
+
+		if(c.gameObject.tag == "gameOver") {
+			SceneManager.LoadScene ("Inicio");
+		}
+
+	}
+	
+	void Update () {
+
+
+	}
+}
